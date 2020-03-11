73eb9714e36330d7f5f7efb9fb5612c0edd0fb04
sebek9 <sebastian.kalinkowski@gmail.com>
2020-03-11 22:56:07 +0100

    script js update

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ $(document).ready(function() {
   }
 
   function handleCardCreationRequest(event) {
-    var requestUrl = trelloApiRoot + 'createTrelloCard';
+    var requestUrl = trelloApiRoot + 'cards';
     var $relatedTaskRow = $(event.target).parents('[data-task-id]');
     var relatedTaskId = $relatedTaskRow.attr('data-task-id');
     var relatedTask = availableTasks[relatedTaskId];
